130527f731b22844d6af20ec3df2dbd49e3d8f87
PKTseng <jk0910948659@Gmail.com>
2025-07-31 22:31:07 +0800

    add SimpleEscrow

---
+++ contract/homework-1.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract SimpleEscrow {
+  address public buyer;
+  address public seller;
+  address public arbiter;
+
+  constructor(address _seller, address _arbiter) {
+    buyer = msg.sender;
+    seller = _seller;
+    arbiter = _arbiter;
+  }
+
+  function deposit() public payable {
+    require(buyer == msg.sender, "only buyer");
+    require(msg.value >= 0, unicode"請存錢");
+    require(address(this).balance == 0, unicode"合約不能有錢");
+  }
+
+  function confirmReceived() public {
+    require(buyer == msg.sender, "only buyer");
+
+    (bool success, ) = payable(seller).call{value: address(this).balance}("");
+    require(success, "transfer fail");
+  }
+
+  function dispute(bool _returnToBuyer) public {
+    require(arbiter == msg.sender, "only arbiter");
+    address recipient = _returnToBuyer ? buyer : seller;
+
+    (bool success, ) = payable(recipient).call{value: address(this).balance}("");
+    require(success, "transfer fail");
+  }
+}
