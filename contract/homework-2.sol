fb90f92244d8dd9dff7f1315feee8505fc02c96b
PKTseng <jk0910948659@Gmail.com>
2025-08-02 16:30:03 +0800

    update

--- contract/homework-2.sol
+++ contract/homework-2.sol
@@ -1,45 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+// 在一定的時間內投資人要投資金額，在一定時間內達標的話項目方可以拿到金額，如果沒達標的話投資人可以自行取款
+
 contract MilestoneBasedCrowdfunding {
-  address manager;
-  address contributor;
-
-  struct Milestone {
-    string description;
-    uint256 amount;
-    bool alreadyPay;
-    uint256 votes;
-  }
-
-  Milestone[] milestoneList;
-  mapping(uint256 => mapping(address => bool)) public milestoneVotes;
+  uint256 constant MINI_VALUE = 100 * 10 ** 18;
+  mapping(address => uint256) public foundersToAmount;
+  address projecter; // 項目方
+  uint256 balance;
 
   uint256 deploymentTimestamp;
-  uint256 lockTime;
+  uint256 lockTimestamp;
 
-  constructor(uint256 _lockTime) {
-    lockTime = _lockTime;
+  constructor(address _projecter, uint256 _lockTimestamp) {
+    projecter = _projecter;
+    deploymentTimestamp = block.timestamp;
+    lockTimestamp = _lockTimestamp;
   }
 
-  modifier onlyManager() {
-    require(msg.sender == manager, "only manager");
+  modifier deadlineTime() {
+    require(block.timestamp > deploymentTimestamp + lockTimestamp, unicode"時間未到");
     _;
   }
 
-  function addMilestone() public view onlyManager {
-    // milestoneVotes.push()
+  // 投資
+  function fund() public payable {
+    require(msg.value > 0, "send more ETH");
+    require(block.timestamp <= deploymentTimestamp + lockTimestamp, "time is end");
+    require(balance < MINI_VALUE, unicode"已達標");
+
+    foundersToAmount[msg.sender] = foundersToAmount[msg.sender] + msg.value;
+    balance += msg.value;
   }
 
-  function contribute() public payable {
-    require(block.timestamp > deploymentTimestamp + lockTime, unicode"尚未到截止時間");
+  // 退款
+  function refund() public deadlineTime {
+    require(balance < MINI_VALUE, "need more eth");
+
+    (bool success, ) = payable(msg.sender).call{value: foundersToAmount[msg.sender]}("");
+    require(success, "transfer fail");
+
+    balance -= foundersToAmount[msg.sender];
+    foundersToAmount[msg.sender] = 0;
   }
 
-  function requestWithdrawal() public view onlyManager {}
+  // 提現
+  function withdraw() public deadlineTime {
+    require(balance >= MINI_VALUE, unicode"投資金額沒達標");
+    require(msg.sender == projecter, "only projecter");
 
-  function voteForMilestone() public view {
-    require(msg.sender == contributor, "only contribute");
+    (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+    require(success, "transfer fail");
+
+    balance = 0; // 因為已經一次提完了所以是0
   }
-
-  function executeWithdrawal() public view onlyManager {}
 }
